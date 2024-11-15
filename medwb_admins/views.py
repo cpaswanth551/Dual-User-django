04b1481dcec50df5fb87b43b049152957023346a
cpaswanth551 <cpaswanth551@gmail.com>
2024-11-15 18:33:37 +0530

    latest code

--- medwb_admins/views.py
+++ medwb_admins/views.py
@@ -3,7 +3,6 @@ from rest_framework.decorators import action
 from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import check_password
 from django.core.mail import send_mail
 from rest_framework.permissions import IsAuthenticated
